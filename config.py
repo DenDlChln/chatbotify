3fbf57a9342b1675748cffbdafbd42d5379ee40e
Denis ChatBotify <denis@chatbotify.ru>
2026-02-02 16:51:18 +0300

    День 4: config.py добавлен

---
+++ config.py
@@ -0,0 +1,11 @@
+CAFE = {
+    "name": "CafeBotify Demo ☕",
+    "phone": "8 (861) 123-45-67",
+    "admin_chat_id": 8388711764:AAFmo3rYdgiBYKC6e9ejFEAdLiQAKkHuWRA,
+    "work_hours": [18, 22],       # 18:00-22:00
+    "menu": {
+        "☕ Кофе": 200,
+        "🍵 Чай": 150,
+        "🥧 Пирог": 100
+    }
+}
